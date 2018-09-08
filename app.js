61a6f9751383f7191e4173f6b88f6962787f9f68
Harshit Pareek <hp1014@nyu.edu>
2018-09-07 18:32:13 -0700

    feat(creating a window)

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
 const electron = require("electron");
 
-console.log("the value of the electron is:");
-console.dir(electron);
+// console.log("the value of the electron is:");
+// console.dir(electron);
+
+const { app, BrowserWindow } = electron;
+
+let win;
+
+const createWindow = () => {
+    win = new BrowserWindow({
+        height: 200,
+        width: 200
+    });
+
+    win.loadFile("./index.html")
+
+    win.webContents.openDevTools();
+
+    win.on("close", () => {
+        win = null;
+    });
+};
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+    if(process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if(win == null) {
+        createWindow();
+    }
+});
